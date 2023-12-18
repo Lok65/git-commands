5a03164058231c8eb17e2426fbb2b8f6ef29e9e9
okaya <okaya6661@outlook.fr>
2023-12-18 15:17:51 +0100

    Feat : Nouvelle Modification Améliorée

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,6 +1,6 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("Conflict Solved");
-        System.out.println("Nouvelle Modification !");
+        System.out.println("Nouvelle Modification Améliorée!");
     }
 }
